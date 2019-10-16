17d0161179fa8c97c6402fad5f1f7a8da762b2c9
Mitskevich Victor <mv2.mx@yandex.ru>
2019-10-16 11:19:54 +0300

    feat: add controlled list

---
+++ dist/listControl.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+  const expandButton = document.querySelector('#section-education__nav__button');
+  const educationList = document.querySelector('#section-education__list');
+
+  expandButton.addEventListener('click', () => {
+    educationList.classList.toggle("toggleList");
+
+
+    let classListArray = Array.from(educationList.classList);
+
+    if (classListArray.includes("toggleList")) {
+      expandButton.textContent = "Expand";
+    } else expandButton.textContent = "Collapse";
+    
+  });
+}
